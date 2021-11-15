ee9722a8d7e303649d83b632397ace1505bd61fc
Shadman <shadman.chowdhury@brainstation23.com>
2021-11-15 15:11:33 +0600

    Intial commit with vuetify and firebase

--- src/main.js
+++ src/main.js
@@ -1,8 +1,26 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import vuetify from "./plugins/vuetify";
+import VueTextareaAutosize from "vue-textarea-autosize";
+import firebase from "firebase/compat/app";
+import "firebase/compat/firestore";
 
-Vue.config.productionTip = false
+Vue.use(VueTextareaAutosize);
+
+Vue.config.productionTip = false;
+firebase.initializeApp({
+  apiKey: "AIzaSyC3nZpXHxMNxzbtROLYDReHUzb5c99qswI",
+  authDomain: "social-site-with-vue.firebaseapp.com",
+  databaseURL: "https://social-site-with-vue-default-rtdb.firebaseio.com",
+  projectId: "social-site-with-vue",
+  storageBucket: "social-site-with-vue.appspot.com",
+  messagingSenderId: "554926265787",
+  appId: "1:554926265787:web:4efd90cbd842cec234e09e",
+});
+
+export const db = firebase.firestore();
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  vuetify,
+  render: (h) => h(App),
+}).$mount("#app");
